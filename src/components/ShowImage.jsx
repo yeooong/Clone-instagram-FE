d1e2228e5d2c891b553cc47893672c9722f1c29a
yeooong <yeong.kim9127@gmail.com>
2023-03-27 20:46:19 +0900

    Test(showImage.jsx): 무한스크롤 테스트

--- src/components/ShowImage.jsx
+++ src/components/ShowImage.jsx
@@ -1,25 +1,47 @@
 import React, { useEffect, useState } from 'react';
 import { getPosts } from '../api/pagenationApi';
-import { pagenation } from '../utils/pagenation';
+
 
 function ShowImage() {
   const [posts, setPosts] = useState([]);
+  const [page, setPage] = useState(1);
 
-  const getPost = async () => {
-    const count = 20;
-    const page = 1;
+  const getPost = async (count, page) => {
 
     const response = await getPosts.get('/instargram_post');
     const data = response.data.filter(post => post.id < count * page);
-    setPosts(data);
+    setPosts([...posts, ...data]);
   };
 
   useEffect(() => {
-    getPost();
-  }, []);
+    getPost(20, { page });
+  }, [page]);
+
+  const handleScroll = () => {
+    const ScrolHeight = document.documentElement.scrollHeight //스크롤 하지 않았을 때의 전체 높이
+    const ScrollTop = document.documentElement.scrollTop //y축 방향으로 스크롤한 거리=스크롤되어 올라간 만큼의 높이
+    const clientHeight = document.documentElement.clientHeight; // 눈에 보이는 만큼의 높이
+
+    console.log("Height:", document.documentElement.scrollHeight);
+    console.log("Top:", document.documentElement.scrollTop);
+    console.log("clientHeight:", clientHeight)
+
+    if (clientHeight + ScrollTop + 1 >= ScrolHeight) {
+      setPage(prev => prev + 1);
+      // setPage(page + 1);
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    // anmount됐을 때
+    return () => window.removeEventListener("scroll", handleScroll)
+  }, []) // 여기서 handScroll에 따라 useEffect를 실행하도록 넣어주기도 하는 듯.
 
   return (
-    <section>
+    <>
+      <section>
       {posts?.map(post => {
         return (
           <div key={post.id}>
@@ -29,7 +51,10 @@ function ShowImage() {
           </div>
         );
       })}
-    </section>
+      </section>
+      <h1>무한스크롤 해죠...</h1>
+    </>
+    
   );
 }
 
